d79f4c013dacc411c946c1272878e7661b4a3ed1
kevinmtanadi1 <kevin.tanadi@ruparupa.com>
2023-06-20 10:35:05 +0700

    Displayed genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,6 +3,7 @@ import useDatas from "./useData";
 export interface Genre {
     id: number;
     name: string;
+    image_background: string;
 }
 
 const useGenres = () => useDatas<Genre>('/genres');
