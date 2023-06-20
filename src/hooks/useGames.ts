700552bc46dbe0602afeb08daacbd8aec6510265
kevinmtanadi1 <kevin.tanadi@ruparupa.com>
2023-06-20 12:59:00 +0700

    Refactor

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from "../App";
 import useDatas from "./useData";
 import { Genre } from "./useGenres";
 
@@ -15,15 +16,14 @@ export interface Game {
     metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) =>
+const useGames = (gameQuery: GameQuery) =>
     useDatas<Game>('/games', {
         params: {
-            genres: selectedGenre?.id,
-            platforms: selectedPlatform?.id
+            genres: gameQuery.genre?.id,
+            platforms: gameQuery.platform?.id
         }
     }, [
-        selectedGenre?.id,
-         selectedPlatform?.id
+        gameQuery
         ]);
 
 export default useGames;
